8127944c8674b6598abbfc2919c1601f996bd450
Tony K Seby <tonyks2003@gmail.com>
2024-12-12 11:03:37 +0530

    Semantic structure defined

--- src/App.js
+++ src/App.js
@@ -1,7 +1,18 @@
 import "./App.css";
+import Nav from "./components/Nav";
+import Header from "./components/Header";
+import Main from "./components/Main";
+import Footer from "./components/Footer";
 
 function App() {
-  return <div className="App">Homepage</div>;
+  return (
+    <>
+      <Nav></Nav>
+      <Header></Header>
+      <Main></Main>
+      <Footer></Footer>
+    </>
+  );
 }
 
 export default App;
---
+++ src/components/Footer.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <>
+      <footer>Footer</footer>
+      <p>Copyright 2024</p>
+    </>
+  );
+};
+
+export default Footer;
